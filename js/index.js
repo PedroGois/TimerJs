4e13aae9de99a75b221cf07a27d620cf02586e47
PedroGois <pedromorais.gois@gmail.com>
2022-09-23 18:39:51 -0300

    Altera cores e adiciona mario

--- js/index.js
+++ js/index.js
@@ -3,6 +3,32 @@ var min = 0
 var sec = 0
 var speed = 1
 var interval
+var buttons = document.querySelector('.botoes')
+var watch = document.getElementById('watch')
+var body = document.querySelector("body")
+var marioimg = document.querySelector(".marioimg")
+
+function changeStyle(value){
+    if (value == 'running'){
+        body.style.background="black"
+        body.style.color="aqua"
+        watch.style.color="aqua"
+        marioimg.style.display="inline"
+        
+        
+    }else if(value=='pause'){
+        body.style.background="red"
+        body.style.color="white"
+        watch.style.color="white"
+        marioimg.style.display="none"
+        
+    }else{
+        body.style.background="aqua"
+        body.style.color="black"
+        watch.style.color="black"
+        marioimg.style.display="none"        
+    }
+}
 
 function twodig(digit){
     if(digit<10){
@@ -13,26 +39,28 @@ function twodig(digit){
 }
 
 function start(){
-    watch()
-    interval = setInterval(watch,1000/speed)
-    document.querySelector('.botoes').innerHTML="<button class='pause-btn' onclick='pause()'>Pausar</button> <button class='stop-btn'  onclick='stop()'>Parar</button>"
-    console.log(botao)
+    watchcontrol()
+    interval = setInterval(watchcontrol,1000/speed)
+    buttons.innerHTML="<button class='pause-btn' onclick='pause()'>Pausar</button> <button class='stop-btn'  onclick='stop()'>Parar</button>"
+    changeStyle('running')
 }
 function pause(){
     clearInterval(interval)
-    document.querySelector('.botoes').innerHTML="<button class='start-btn' onclick='start()'>Retomar</button> <button class='stop-btn'  onclick='stop()'>Parar</button>"
+    buttons.innerHTML="<button class='start-btn' onclick='start()'>Retomar</button> <button class='stop-btn'  onclick='stop()'>Parar</button>"
+    changeStyle('pause')
 }
 function stop(){
     clearInterval(interval)
     hr = 0
     min = 0
     sec = 0
-    document.getElementById('watch').innerHTML= twodig(hr)+":"+twodig(min)+":"+twodig(sec)
-    document.querySelector('.botoes').innerHTML="<button class='start-btn' onclick='start()'>Iniciar</button>"
+    watch.innerHTML= twodig(hr)+":"+twodig(min)+":"+twodig(sec)
+    buttons.innerHTML="<button class='start-btn' onclick='start()'>Iniciar</button>"
+    changeStyle(false)
 }
 
 
-function watch(){
+function watchcontrol(){
     sec++
     if (sec==60){
         min++
@@ -42,7 +70,7 @@ function watch(){
             min=0
         }
     }
-    document.getElementById('watch').innerHTML= twodig(hr)+":"+twodig(min)+":"+twodig(sec)
+    watch.innerHTML= twodig(hr)+":"+twodig(min)+":"+twodig(sec)
 }
 
 /* Alterando velocidade */
